725bb6f75758f78db84663f377671ef3c5c3ab5d
Illusion52 <andreytalaba52@gmail.com>
2022-10-07 14:11:01 +0300

    node_modules folder!

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-const express = require('express')
+
+const express= require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
@@ -13,13 +14,16 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
-
 app.use('/', indexRouter)
 
-app.listen(3000)
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to MongoDB'))
+
+
+
+app.listen(process.env.PORT || 3000)
